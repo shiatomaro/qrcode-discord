18d4774ae15952aad74bc85c7a764e7712d8e2de
shiatomaro <officialtrishiagavile@gmail.com>
2025-03-11 13:02:24 +0800

    added as pakage

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/shiatomaro/qrcode-discord/internal/qrgen"
 )
 
 func main() {
+
 	userID := flag.String("user", "", "Discord User ID")
 	invite := flag.String("invite", "", "Discord Invite Code")
-	output := flag.String("o", "discord_qr.png", "Outful filename")
+	output := flag.String("o", "discord_qr.png", "Output filename")
 	size := flag.Int("size", 256, "QR Code size in pixels")
-
 	flag.Parse()
 
+	var input string
 	if *userID == "" && *invite == "" {
-		fmt.Println("Error: Provide a Discord user ID or Invite Code.")
-		flag.Usage()
+		fmt.Print("Enter Discord User ID or Invite Code:")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+	} else {
+		if *userID != "" {
+			input = *userID
+		} else {
+			input = *invite
+		}
+	}
+
+	if input == "" {
+		fmt.Println("Error: No Input Provided")
 		os.Exit(1)
 	}
-
-	var content string
-	if *userID != "" {
-		content = fmt.Sprintf("https://discord.com/users/%s", *userID)
-	} else {
-		content = fmt.Sprintf("https://disocrd.gg/%s", *invite)
-	}
-
-	err := qrgen.GenerateQR(content, *output, *size)
+	err := qrgen.GenerateQRFromInput(input, *output, *size)
 	if err != nil {
 		log.Fatal(err)
 	}
--- internal/qrgen/qrgen.go
+++ internal/qrgen/qrgen.go
@@ -6,7 +6,17 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
-func GenerateQR(content string, filename string, size int) error {
+func GenerateQRFromInput(userInput string, filename string, size int) error {
+	var content string
+	if len(userInput) == 0 {
+		return fmt.Errorf("no input provided")
+	}
+	if len(userInput) > 15 {
+		content = fmt.Sprintf("https://discord.com/users/%s", userInput)
+	} else {
+		content = fmt.Sprintf("https://discord.gg/%s", userInput)
+	}
+
 	err := qrcode.WriteFile(content, qrcode.Medium, size, filename)
 	if err != nil {
 		return err
